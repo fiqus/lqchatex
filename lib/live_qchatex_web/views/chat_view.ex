901aac5a3c358cba3d9efb86d3698e567831983c
Diego Calero <dcalero@fiqus.coop>
2019-06-08 22:45:16 -0300

    Formatting messages timestamps for chat view.

--- lib/live_qchatex_web/views/chat_view.ex
+++ lib/live_qchatex_web/views/chat_view.ex
@@ -3,4 +3,18 @@ defmodule LiveQchatexWeb.ChatView do
 
   def ellipsis(true), do: "<span class=\"ellipsis\"></span>"
   def ellipsis(false), do: nil
+
+  def parse_timestamp(ts) when is_integer(ts) do
+    {:ok, %DateTime{hour: hour, minute: minute, second: second}} = DateTime.from_unix(ts)
+    format_date_values([hour, minute, second], ":")
+  end
+
+  def parse_timestamp(_), do: "-"
+
+  defp format_date_values(values, glue),
+    do:
+      values
+      |> Enum.map(&to_string/1)
+      |> Enum.map(&String.pad_leading(&1, 2, "0"))
+      |> Enum.join(glue)
 end
