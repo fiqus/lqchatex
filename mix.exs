8524b8928ee6e6224f81db9c24931fb799179048
Diego Calero <dcalero@fiqus.coop>
2019-11-17 17:22:29 -0300

    Updating deps.

--- mix.exs
+++ mix.exs
@@ -56,7 +56,7 @@ defmodule LiveQchatex.MixProject do
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.3"},
+      {:phoenix_live_view, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
